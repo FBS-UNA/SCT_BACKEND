019f8e46fe44d65b7f87259d8ae2e2b9177967d8
Jared Aguilar Muñoz <70741513+Jared2910@users.noreply.github.com>
2023-02-13 21:38:33 -0600

    Jared (#1)
    * Add agregarAreas
    
    Se agrego la funcionalidad de agregar Areas nuevas
    
    * UPDATE and DELETE AREAS
    
    Se implementaron las peticiones para eliminar y actualizar las areas
    
    * GET AREAS
    
    Se implemento la peticion para traer todas las areas de la BD

---
+++ routes/areas.js
@@ -0,0 +1,34 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { agregarArea, eliminarArea, actualizarArea, getAreas } = require('../controllers/areas.controller');
+const { validarCampos } = require('../middlewares/validar-campos');
+
+
+
+const router = Router();
+
+// GET AREAS
+router.get('/', getAreas);
+
+// ADD AREA
+router.post('/agregar',[
+    check('NOMBRE_AREA', 'El nombre del área es obligatorio').notEmpty(),
+    check('FECHA', 'La fecha de creacion del área es obligatoria').notEmpty().isDate(),
+    validarCampos
+], agregarArea);
+
+// UPDATE AREA
+router.post('/actualizar',[
+    check('ID_AREA', 'El ID_AREA es obligatorio').notEmpty().isNumeric(),
+    check('NOMBRE_AREA', 'El NOMBRE_AREA es obligatorio').notEmpty(),
+    validarCampos
+],actualizarArea);
+
+// DELETE AREA
+router.post('/eliminar',[
+    check('ID_AREA', 'El ID_AREA es obligatorio').notEmpty().isNumeric(),
+    validarCampos
+],eliminarArea);
+
+
+module.exports = router;
