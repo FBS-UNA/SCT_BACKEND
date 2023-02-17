cc2ffdb4d7ce1c059e1c80b08cf73ea0011bed0f
Jared Aguilar Muñoz <70741513+Jared2910@users.noreply.github.com>
2023-02-17 03:19:14 -0600

    Jared (#6)
    * Se creó la peticion para acceder al registro de entrada
    
    * SOME ERRORS FIXED
    
    Se arreglo un comando erroneo en el README
    
    Se reacomodo la peticion del registro de entrada para que funcione con el objeto Date de Oracle
    
    * validar JWT

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { crearUsuario, loginUsuario } = require('../controllers/auth.controller');
+const { crearUsuario, loginUsuario, validarUsuario } = require('../controllers/auth.controller');
 const { validarCampos } = require('../middlewares/validar-campos');
-
+const { validarJWT } = require('../middlewares/validar-JWT');
 
 const router = Router();
 
@@ -20,9 +20,13 @@ router.post( '/register', [
 
 // Login
 router.post('/login',[
-    check( 'CEDULA', 'La cedula es obligatoria').notEmpty().isLength( {min: 9} ),
+    check( 'CEDULA', 'La cedula es obligatoria').notEmpty().isLength( {min: 9} ).withMessage('La cedula debe tener al menos 9 dígitos'),
     check( 'CONTRASENA', 'La contraseña es obligatoria').notEmpty(),
     validarCampos
 ], loginUsuario)
+
+
+// Validar Token
+router.get('/validartoken', validarJWT, validarUsuario);
 
 module.exports = router;
