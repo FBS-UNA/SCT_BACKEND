6637dffe99da821800d0414b8ea93f16024c5b26
Biron Aguilar Rodríguez <biron2465@gmail.com>
2023-04-15 02:56:50 -0600

    Se elimina el metodo hecho anteriormente
    Porque no va en este controler

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -154,39 +154,6 @@ const validarUsuario = async (req = request, res = response) => {
 
 }
 
-const getUsuario = async (req = request, res = response)=>{
-
-    const sql = 'SELECT * FROM USUARIOS';
-    const usuarios = [];
-
-    try {
-
-        let dbresponse = await BD.dbConnection(sql, [], false);
-
-        dbresponse.rows.map((data)=>{
-            const usuario = {}
-            dbresponse.metaData.map(({name}, index)=>{
-                usuario[name] = data[index];
-            })
-            usuarios.push(usuario);
-        });
-
-
-        return res.json({
-            OK:true,
-            USUARIOS: usuarios
-        });
-        
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            OK: false,
-            MSG: 'Por favor hable con el administrador'
-        });
-    }
-
-};
-
 
 module.exports = {
     crearUsuario,
