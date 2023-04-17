c38e8abe5a887bd1643212ed99d7147b84a87631
Biron Aguilar Rodríguez <biron2465@gmail.com>
2023-04-17 02:44:09 -0600

    Feature_Crear_Testear_metodos_cargar_roles_usuarios closed #34
    Se crea el metodo de getRolesUsuarios que traer los roles que tienen los usuarios asignados
    Se realiza las prueba en postman

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -34,6 +34,41 @@ const getUsuarios= async (req = request, res = response)=>{
 
 };
 
+const getRolesUsuarios= async (req = request, res = response)=>{
+
+    const { CEDULA_USUARIO } = req.body;
+    const sql = 'SELECT NOMBRE_ROL FROM USUARIOS_ROLES WHERE CEDULA_USUARIO = :CEDULA_USUARIO';
+    const roeles_usuarios = [];
+
+    try {
+
+        let dbresponse = await BD.dbConnection(sql, [CEDULA_USUARIO], false);
+
+        dbresponse.rows.map((data)=>{
+            const rol_usuario = {}
+            dbresponse.metaData.map(({name}, index)=>{
+                rol_usuario[name] = data[index];
+            })
+            roeles_usuarios.push(rol_usuario);
+        });
+        
+        return res.json({
+            OK:true,
+            USUARIOS: roeles_usuarios
+        });
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            OK: false,
+            MSG: 'Por favor hable con el administrador'
+        });
+    }
+
+};
+
+
 module.exports = {
-    getUsuarios
+    getUsuarios,
+    getRolesUsuarios
 }
