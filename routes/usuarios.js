5e98d36f25a52bcd8509b94cfa00c97d158822e6
Biron Aguilar Rodríguez <71033592+Biron2465@users.noreply.github.com>
2023-04-17 02:47:41 -0600

    Feature_Crear_Testear_metodos_cargar_roles_usuarios closed #34 (#35)
    Se crea el metodo de getRolesUsuarios que traer los roles que tienen los usuarios asignados
    Se realiza las prueba en postman

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const { getUsuarios } = require('../controllers/usuarios.controller');
+const { getRolesUsuarios } = require('../controllers/usuarios.controller');
 
 
 
@@ -7,5 +8,8 @@ const router = Router();
 
 // GET USUARIOS
 router.get('/', getUsuarios);
+
+// GET ROLES DE USUARIOS
+router.get('/rolesusuario', getRolesUsuarios)
 
 module.exports = router;
