88b2ccc80efe615dae4f85e01b634332838fffff
vito2312 <93292401+vito2312@users.noreply.github.com>
2023-04-20 01:01:05 -0600

    Consulta asignacion roles (#38)
    * #19
    
    Peticiones de roles
    
    * #19
    
    ya esta listo esta vara
    
    * Feature_ConsultaAsignacionDeRoles closed#19
    
    Ya se asignan n roles al usuario
    
    * Select de Prueba
    
    Consulta usuario con roles

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -66,7 +66,65 @@ const getRolesUsuarios= async (req = request, res = response)=>{
     }
 
 };
-
+ 
+//PETICION QUE SE USARA PARA EL REGISTRO DEL TRAMITE
+/*
+  getUsuarioConRoles = async (req = request, res = response) => {
+    const { CEDULA_USUARIO } = req.body;
+  
+    try {
+      const sql = `SELECT U.CEDULA, U.NOMBRE, U.APELLIDO_1, U.APELLIDO_2, U.FECHA_NAC, UR.NOMBRE_ROL
+                   FROM USUARIOS U
+                   JOIN USUARIOS_ROLES UR ON U.CEDULA = UR.CEDULA_USUARIO
+                   WHERE U.CEDULA = :CEDULA_USUARIO`;
+      const dbresponse = await BD.dbConnection(sql, [CEDULA_USUARIO], false);
+  
+      const usuario = {
+        CEDULA: dbresponse.rows[0][0],
+        NOMBRE: dbresponse.rows[0][1],
+        APELLIDO_1: dbresponse.rows[0][2],
+        APELLIDO_2: dbresponse.rows[0][3],
+        ROLES: dbresponse.rows.map((row) => ({ NOMBRE_ROL: row[5] })),
+      };
+  
+      return res.json({
+        OK: true,
+        USUARIO: usuario,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        OK: false,
+        MSG: "Por favor hable con el administrador",
+      });
+    }
+  };
+  */
+// PETICION PARA ASIGNARLE ROLES AL USUARIO
+    asignarRolesUsuario = async (req = request, res = response) => {
+    const { CEDULA_USUARIO, ROLES } = req.body;
+  
+    try {
+      for (const rol of ROLES) {
+        const { NOMBRE_ROL } = rol;
+        const sql = 'INSERT INTO USUARIOS_ROLES (NOMBRE_ROL, CEDULA_USUARIO) VALUES (:NOMBRE_ROL, :CEDULA_USUARIO)';
+        await BD.dbConnection(sql, [NOMBRE_ROL, CEDULA_USUARIO], true);
+      }
+  
+      return res.json({
+        OK: true,
+        MSG: 'Roles asignados correctamente al usuario',
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        OK: false,
+        MSG: 'Por favor hable con el administrador',
+      });
+    }
+  };
+  
+  
 
 module.exports = {
     getUsuarios,
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,13 +3,16 @@ const { getUsuarios } = require('../controllers/usuarios.controller');
 const { getRolesUsuarios } = require('../controllers/usuarios.controller');
 
 
-
 const router = Router();
 
 // GET USUARIOS
 router.get('/', getUsuarios);
 
 // GET ROLES DE USUARIOS
-router.get('/rolesusuario', getRolesUsuarios)
+router.get('/rolesusuario', getRolesUsuarios);
+
+router.get('/rolesporusuario', getUsuarioConRoles);
+
+router.post('/asignarroles', asignarRolesUsuario);
 
 module.exports = router;
