e7a0ea3b5e95d7ceaf9f773fc2d83c5702f0c858
vito2312 <alvarobu23@gmail.com>
2023-04-19 22:08:57 -0600

    #19
    Peticiones de roles

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const { getUsuarios } = require('../controllers/usuarios.controller');
-const { getRolesUsuarios } = require('../controllers/usuarios.controller');
-
+const { getRolesUsuarios, asignarRolesUsuario } = require('../controllers/usuarios.controller');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 
 const router = Router();
@@ -10,6 +10,12 @@ const router = Router();
 router.get('/', getUsuarios);
 
 // GET ROLES DE USUARIOS
-router.get('/rolesusuario', getRolesUsuarios)
+router.get('/rolesusuario', getRolesUsuarios);
+
+router.get('/rolesporusuario',getUsuarioConRoles);
+
+
+router.post('/asignarRoles',asignarRolesUsuario);
+
 
 module.exports = router;
