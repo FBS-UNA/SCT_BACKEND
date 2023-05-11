caaa51f286c31b31ffec7dc0b9c620be83d726bf
Jared2910 <jared291095@gmail.com>
2023-05-11 02:11:13 -0600

    Creando las rutas

---
+++ routes/chart.js
@@ -0,0 +1,21 @@
+const { Router } = require('express');
+const { check, header } = require('express-validator');
+const { getDonutTramites, getDonutAreas, getTotalRegistros, getCantidadReportesPorMes, getCantidadReportesPorAnnio } = require('../controllers/chart.controlle');
+
+
+const router = Router();
+
+// GET DONUT TRAMITES
+router.get('/donut-tramites', getDonutTramites);
+
+// GET DONUT AREAS
+router.get('/donut-areas', getDonutAreas);
+
+// GET TOTAL DE REGISTROS
+router.get('/total-registros', getTotalRegistros);
+
+// GET TOTAL DE REGISTROS POR MES
+router.get('/total-registros/mes', getCantidadReportesPorMes);
+
+// GET TOTAL DE REGISTROS POR ANNIO
+router.get('/total-registros/annio', getCantidadReportesPorAnnio);
