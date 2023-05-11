6e756e979967583ad14bcd24854475c8ec15d212
Biron Aguilar Rodríguez <biron2465@gmail.com>
2023-05-11 00:44:20 -0600

    Funciona lo de que se seleccionan los roles del usuario

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -4,7 +4,7 @@ const { response, request } = require('express');
 
 const getUsuarios= async (req = request, res = response)=>{
 
-    const sql = 'SELECT * FROM USUARIOS';
+    const sql = 'SELECT * FROM VISTA_USUARIOS_ROLES';
     const usuarios = [];
 
     try {
@@ -34,40 +34,6 @@ const getUsuarios= async (req = request, res = response)=>{
 
 };
 
-
-//PETICION QUE SE USARA PARA EL REGISTRO DEL TRAMITE
-/*
-  getUsuarioConRoles = async (req = request, res = response) => {
-    const { CEDULA_USUARIO } = req.body;
-  
-    try {
-      const sql = `SELECT U.CEDULA, U.NOMBRE, U.APELLIDO_1, U.APELLIDO_2, U.FECHA_NAC, UR.NOMBRE_ROL
-                   FROM USUARIOS U
-                   JOIN USUARIOS_ROLES UR ON U.CEDULA = UR.CEDULA_USUARIO
-                   WHERE U.CEDULA = :CEDULA_USUARIO`;
-      const dbresponse = await BD.dbConnection(sql, [CEDULA_USUARIO], false);
-  
-      const usuario = {
-        CEDULA: dbresponse.rows[0][0],
-        NOMBRE: dbresponse.rows[0][1],
-        APELLIDO_1: dbresponse.rows[0][2],
-        APELLIDO_2: dbresponse.rows[0][3],
-        ROLES: dbresponse.rows.map((row) => ({ NOMBRE_ROL: row[5] })),
-      };
-  
-      return res.json({
-        OK: true,
-        USUARIO: usuario,
-      });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-        OK: false,
-        MSG: "Por favor hable con el administrador",
-      });
-    }
-  };
-  */
 // PETICION PARA ASIGNARLE ROLES AL USUARIO
     asignarRolesUsuario = async (req = request, res = response) => {
     const { CEDULA_USUARIO, ROLES } = req.body;
